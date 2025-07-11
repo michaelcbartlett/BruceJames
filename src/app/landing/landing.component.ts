36af2bd4892a845fe6447cc4927442a708964c9c
Michael Bartlett <michael.bartlett@pershingtech.com>
2025-07-11 16:11:55 -0400

    trying gtag

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare function gtag(command: 'event', eventName: string, params?: { [key: string]: any }): void;
+
 @Component({
   selector: 'app-landing',
   imports: [],
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+  handleBuyMarcoClick() {
+    gtag('event', 'buy_marco', {
+      event_category: 'engagement',
+      event_label: 'Buy Marco Button',
+      value: 1
+    });
+
+    // Wait 300ms to ensure GA logs the event before redirecting
+    setTimeout(function () {
+      window.location.href = "https://brucejames.gumroad.com/l/marco?wanted=true";
+    }, 300);
+  }
 }
