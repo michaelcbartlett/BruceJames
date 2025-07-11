89da549fb7e92518115a3a274d12c8c9fb93c4f1
Michael Bartlett <michael.bartlett@pershingtech.com>
2025-07-11 16:16:07 -0400

    second attempt

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -10,15 +10,19 @@ declare function gtag(command: 'event', eventName: string, params?: { [key: stri
 })
 export class LandingComponent {
   handleBuyMarcoClick() {
-    // gtag('event', 'buy_marco', {
-    //   event_category: 'engagement',
-    //   event_label: 'Buy Marco Button',
-    //   value: 1
-    // });
+    if (typeof window !== 'undefined' && Array.isArray((window as any).dataLayer)) {
+        (window as any).dataLayer.push({
+          event: 'buy_marco',
+          event_category: 'engagement',
+          event_label: 'Buy Marco Button',
+          value: 1
+        });
+      } else {
+        console.warn('dataLayer is not defined');
+      }
 
-    // // Wait 300ms to ensure GA logs the event before redirecting
-    // setTimeout(function () {
-    //   window.location.href = "https://brucejames.gumroad.com/l/marco?wanted=true";
-    // }, 300);
+      setTimeout(() => {
+        window.location.href = 'https://brucejames.gumroad.com/l/marco?wanted=true';
+      }, 300);
   }
 }
