9090099421605894f593734825ecbd8005cea395
Michael Bartlett <michael.bartlett@pershingtech.com>
2025-07-11 16:14:07 -0400

    bypass

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -10,15 +10,15 @@ declare function gtag(command: 'event', eventName: string, params?: { [key: stri
 })
 export class LandingComponent {
   handleBuyMarcoClick() {
-    gtag('event', 'buy_marco', {
-      event_category: 'engagement',
-      event_label: 'Buy Marco Button',
-      value: 1
-    });
+    // gtag('event', 'buy_marco', {
+    //   event_category: 'engagement',
+    //   event_label: 'Buy Marco Button',
+    //   value: 1
+    // });
 
-    // Wait 300ms to ensure GA logs the event before redirecting
-    setTimeout(function () {
-      window.location.href = "https://brucejames.gumroad.com/l/marco?wanted=true";
-    }, 300);
+    // // Wait 300ms to ensure GA logs the event before redirecting
+    // setTimeout(function () {
+    //   window.location.href = "https://brucejames.gumroad.com/l/marco?wanted=true";
+    // }, 300);
   }
 }
