4985f322ef6c5053ea0ef670899ab664347bd3a6
Michael Bartlett <michael.bartlett@pershingtech.com>
2025-07-11 16:57:23 -0400

    added the tags

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -25,4 +25,39 @@ export class LandingComponent {
         window.location.href = 'https://brucejames.gumroad.com/l/marco?wanted=true';
       }, 300);
   }
+
+  handleDownloadWindowsClick() {
+        if (typeof window !== 'undefined' && Array.isArray((window as any).dataLayer)) {
+        (window as any).dataLayer.push({
+          event: 'dl_marco_windows_click',
+          event_category: 'engagement',
+          event_label: 'Download Marco Windows Button',
+          value: 1
+        });
+      } else {
+        console.warn('dataLayer is not defined');
+      }
+
+      setTimeout(() => {
+        window.location.href = 'https://f005.backblazeb2.com/file/BruceJames-Marco/Marco_v1_1_6_Windows.zip';
+      }, 300);
+
+  }
+
+  handleDownloadmacOSClick() {
+    if (typeof window !== 'undefined' && Array.isArray((window as any).dataLayer)) {
+        (window as any).dataLayer.push({
+          event: 'dl_marco_macos_click',
+          event_category: 'engagement',
+          event_label: 'Download Marco macOS Button',
+          value: 1
+        });
+      } else {
+        console.warn('dataLayer is not defined');
+      }
+
+      setTimeout(() => {
+        window.location.href = 'https://f005.backblazeb2.com/file/BruceJames-Marco/Marco_v1_1_5_macOS.zip';
+      }, 300);
+  }
 }
